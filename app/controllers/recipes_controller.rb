71ca19b715e0ad59cd861c87f7e9439adc641086
github username <github email address>
2019-04-21 16:02:46 -0400

    add active_storage for recipe model and controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -71,6 +71,6 @@ class RecipesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def recipe_params
-      params.require(:recipe).permit(:title, :description, :level, :servings, :category_id)
+      params.require(:recipe).permit(:title, :description, :level, :servings, :category_id, :image)
     end
 end
